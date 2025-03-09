37414fb74ae84b4bb34cba2f699ef1f95205c836
srodan12 <112351302+srodan12@users.noreply.github.com>
2025-03-09 17:59:53 +0300

    Delete test_pyttsx3.py

--- test_pyttsx3.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-try:
-    engine = pyttsx3.init()
-    engine.say("Hello, world!")
-    engine.runAndWait()
-    print("pyttsx3 initialized successfully")
-except Exception as e:
-    print(f"Error initializing pyttsx3: {e}")
